7a54a6269775b9e4d92279c7a828e58d70260794
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-25 15:37:52 -0300

    feat: Add initial visualization for start page in PWA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Ranking } from './views'
+import styles from './App.module.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className={styles.centralizer}>
+            <Switch>
+                <Route path='/' component={Ranking}/>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
---
+++ src/views/Ranking/Ranking.jsx
@@ -0,0 +1,12 @@
+import { Rank, Filter } from "../../components"
+import styles from './Ranking.module.css'
+
+const Ranking = () => {
+    return (
+        <div className={styles.mainContainer}>
+            
+        </div>
+    )
+}
+
+export default Ranking
