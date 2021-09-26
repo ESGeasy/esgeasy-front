9718fd0f6ae57f4f5d9d08a2fc16fce5477e78a7
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-26 18:36:15 -0300

    feat: Add logo to homepage

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -21,58 +20,65 @@ const theme = createTheme({
 export default function SignIn() {
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs" >
-      <CssBaseline />
-        <Box bgcolor="white"
-          sx={{
-            borderRadius: '12px',
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main', marginTop: '20px' }}>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1, display:'flex', flexDirection: 'column' }}>
-            <TextField
-              margin="normal"
-              required
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              href="/forms"
-            >
-              Sign In
-            </Button>
+    <div style={{"display": "flex",
+                 "flexDirection": "column",
+                 "alignItems": "center",
+                 "justifyContent": "center",
+                 "width": "56.25vh"}}>
+      <img src='https://cdn.discordapp.com/attachments/891129799974719559/891799763291365396/image0.png'
+           alt='logo' />
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs" >
+        <CssBaseline />
+          <Box bgcolor="white"
+            sx={{
+              borderRadius: '12px',
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box mb={3} />
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1, display:'flex', flexDirection: 'column' }}>
+              <TextField
+                margin="normal"
+                required
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                href="/forms"
+              >
+                Sign In
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 }
