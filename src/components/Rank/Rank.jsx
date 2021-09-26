e73d6075ab7c542e45dc08ed4404606f5805fc4c
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-09-26 12:14:07 -0300

    fix: Fixing rank object access

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Rank = ({ companies }) => {
     const topScore = companies[0].score
-    const bottomScore = companies[companies.length-1].score
+    const bottomScore = companies[Object.keys(companies).length - 1].score
     const normalizedDenominator = (topScore - bottomScore)
 
     return (
@@ -21,12 +21,13 @@ const Rank = ({ companies }) => {
                 <span>Score - Company</span>
                 <span>Know More</span>
             </div>
-            {companies.map(({ name, score, id, ...rest }) => {
-                let normalizedScore = (score - bottomScore) / normalizedDenominator * 100
+            {Object.keys(companies).map((id) => {
+                let company = companies[id]
+                let normalizedScore = (company.score - bottomScore) / normalizedDenominator * 100
                 let color = getGreenToRed(normalizedScore)
 
                 return (
-                    <Link to={"/company/"+ id} key={id} className={css`
+                    <Link to={"/company/" + company.id} key={company.id} className={css`
                         background-color: ${color};
                         font-size: 1em;
                         display: flex;
@@ -41,9 +42,9 @@ const Rank = ({ companies }) => {
                             opacity: 0.8;
                         }
                     `}>
-                        <span>{Math.floor(score)+' - '+name}</span>
+                        <span>{Math.floor(company.score) + ' - ' + company.company_name}</span>
                         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-                            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/>
+                            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
                         </svg>
                     </Link>
                 )
