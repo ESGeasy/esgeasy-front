575547517ca2fc3abb454b5a1406a5d3a6459988
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-26 03:33:33 -0300

    feat: forms

---
+++ src/views/Forms/Forms.jsx
@@ -0,0 +1,137 @@
+import { BuzzFeedQuiz } from "react-buzzfeed-quiz";
+import styles from './Forms.module.css'
+import "react-buzzfeed-quiz/lib/styles.css";
+import { Redirect } from "react-router-dom";
+import { useState } from "react";
+
+export default function Forms() {
+    const [redirect, setRedirect] = useState(false);
+
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
+
+    return (
+        <div className={styles.mainContainer}>
+            <BuzzFeedQuiz
+            title={"Diga o que te move!"}
+            autoScroll={true}
+            questions={[
+                {
+                    question: "Qual problema da humanidade é mais urgente para você?",
+                    answers: [
+                        {
+                            answer: "Mudanças Climáticas",
+                            resultID: 0,
+                        },
+                        {
+                            answer: "Desigualdade Social",
+                            resultID: 1,
+                        },
+                        {
+                            answer: "Corrupção em instituições Públicas",
+                            resultID: 2,
+                        },
+                        {
+                            answer: "Todas as anteriores",
+                            resultID: 3,
+                        }
+                    ],
+                },
+                {
+                    question: "O que faz você gostar de uma empresa?",
+                    answers: [
+                        {
+                            answer: "Investimento em inovação e novas tecnologias",
+                            resultID: 0,
+                        },
+                        {
+                            answer: "Preocupação com o meio ambiente",
+                            resultID: 1,
+                        },
+                        {
+                            answer: "Cuidado e investimento nos seus funcionários",
+                            resultID: 2,
+                        },
+                        {
+                            answer: "Todas as anteriores",
+                            resultID: 3,
+                        }
+                    ],
+                },
+                {
+                    question: "Qual dessas empresas você investiria?",
+                    answers: [
+                        {
+                            answer: "Óleo, Gás e Combustíveis fósseis",
+                            resultID: 0,
+                        },
+                        {
+                            answer: "Metais e mineração",
+                            resultID: 1,
+                        },
+                        {
+                            answer: "Construção de rodovias ",
+                            resultID: 2,
+                        },
+                        {
+                            answer: "Têxteis e Vestuários",
+                            resultID: 3,
+                        }
+                    ],
+                },
+                {
+                    question: "Uma empresa perfeita deve:",
+                    answers: [
+                        {
+                            answer: "Não possuir envolvimento com corrupção",
+                            resultID: 0,
+                        },
+                        {
+                            answer: "Possuir diversidade no quadro de funcionários",
+                            resultID: 1,
+                        },
+                        {
+                            answer: "Reinvestir seus lucros em comunidades locais",
+                            resultID: 2,
+                        },
+                        {
+                            answer: "Reciclar todos os seus resíduos",
+                            resultID: 3,
+                        }
+                    ],
+                },
+                {
+                    question: "Qual dessas empresas você investiria se tivesse oportunidade?",
+                    answers: [
+                        {
+                            answer: "Apple",
+                            onAnswerSelection: () =>
+                            setRedirect(true),
+                            resultID: 0,
+                        },
+                        {
+                            answer: "Petrobrás",
+                            onAnswerSelection: () =>
+                            setRedirect(true),
+                            resultID: 1,
+                        },
+                        {
+                            answer: "Caixa economica federal",
+                            onAnswerSelection: () =>
+                            setRedirect(true),
+                            resultID: 2,
+                        },
+                        {
+                            answer: "Startups inovadoras",
+                            onAnswerSelection: () =>
+                            setRedirect(true),
+                            resultID: 3,
+                        }
+                    ],
+                }       
+            ]}
+            />
+        </div>
+    );
+};
--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,4 @@
 export { default as Ranking } from './Ranking/Ranking'
 export { default as CompanyDetail } from './CompanyDetail/CompanyDetail'
 export { default as Login } from './Login/Login'
-export { default as Landing } from './Landing/Landing'
+export { default as Forms } from './Forms/Forms'
