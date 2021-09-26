b9069155bcf523d32c91df29980c6881a5b946eb
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-26 03:32:11 -0300

    feat: correct ranking connection to api (need to fix backend)

--- src/views/Ranking/Ranking.jsx
+++ src/views/Ranking/Ranking.jsx
@@ -1,6 +1,8 @@
 import { Rank, Filter } from "../../components"
-import { useState } from 'react'
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { CircularProgress } from '@mui/material';
+import { useLocation, Link } from "react-router-dom";
+import { getFutureRanking, getCurrentRanking } from "../../utils/api"
 import styles from './Ranking.module.css'
 import queryString from 'query-string'
 
@@ -19,15 +21,48 @@ const rankTypeValues = [
     'Governance & Economic Dimension'
 ]
 
-const Ranking = ({ endpoint }) => {
+const Ranking = ({ future }) => {
+    const [rankings, setRankings] = useState([])
     const location = useLocation();
     const parsed = queryString.parse(location.search);
+    let title = "Current Ranking"
+    let buttonText = 'Prediction Ranking' 
+    let otherLink = '/futureRanking'
+    let method = getCurrentRanking
+
+    if (future) {
+        method = getFutureRanking
+        title = "Prediction Ranking"
+        buttonText = 'Current Ranking'
+        otherLink = '/ranking'
+    }
     
     const [chosenSector, setChosenSector] = useState(parsed.sector || "All")
     const [chosenRankType, setChosenRankType] = useState(parsed.rank || "S&P Global ESG Score")
 
+    useEffect(() => {
+        setRankings([])
+        method(chosenSector, chosenRankType).then((response) =>{
+            setRankings(response.data)       
+        }).catch(()=>{
+            setRankings([])
+        })
+        }, [method, chosenSector, chosenRankType])
+    
+
     return (
         <div className={styles.mainContainer}>
+            <h1>
+                {title}
+            </h1>
+            <Link to={otherLink} style={{ "textDecoration": "none" }}>
+                <button className={styles.myButton}>
+                    {buttonText}
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                        <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/>
+                    </svg>
+                </button>
+            </Link>
             <div className={styles.filtersContainer}>
                 <Filter name="Sector"
                         posibleValues={sectorValues}
@@ -38,7 +73,9 @@ const Ranking = ({ endpoint }) => {
                         defaultValue={chosenRankType}
                         handler={setChosenRankType} />
             </div>
-            <Rank sector={chosenSector} rankType={chosenRankType} endpoint={endpoint} />
+            { rankings.length===0 ? <CircularProgress /> : 
+                <Rank companies={rankings} />
+            }
         </div>
     )
 }
