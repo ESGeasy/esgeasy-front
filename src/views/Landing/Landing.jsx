f444a9fa2cd7f89d91d90c16fed4fad7ba60e168
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-26 00:13:03 -0300

    feat: start to add landing

---
+++ src/views/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import styles from './Landing.module.css';
+
+const Landing = () => {
+    return (
+        <div className={styles.mainContainer}>
+            <h1>Company</h1>
+        </div>
+    );
+}
+
+export default Landing
