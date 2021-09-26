a5cd0e5dcfbb381b0c525593d26fd6d18f2f18e1
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-09-26 14:54:09 -0300

    feat: Adding environment page

--- src/views/Environment/Environment.jsx
+++ src/views/Environment/Environment.jsx
@@ -1,21 +1,27 @@
 import styles from './Environment.module.css'
-import logo from "../../images/environmental_logo.png"
+import environment_logo from "../../images/environmental_logo.png"
+import { Link } from "react-router-dom";
 
 const response = {
     id: 1,
-    name: "Ambientalista"
+    name: "Investidor Ambientalista",
+    alt: "Investidor Ambientalista",
+    text: "Você acredita que o futuro do planeta depende diretamente da preservação do meio ambiente. Existem empresas que estão trazendo inovações nessa área e outras que estão revolucionando o seu modelo de negócio para garantir um futuro melhor para todos. Venha ver as nossas recomendações de empresas que possuem a área ambiental como prioridade."
 }
 
 export default function Environment({id}) {
 
     return (
         <div className={styles.mainContainer}>
-            <img src={logo} alt="PAV Main Logo" className={styles.logo} />
+            <img src={environment_logo} alt={response.alt} className={styles.logo} />
             <h1>{response.name}</h1>
-            <p> Lorem Ipsum </p>
-            <p> Lorem Ipsum </p>
-            <p> Lorem Ipsum </p>
-            <p> Lorem Ipsum </p>
+            <p className={styles.text}> {response.text} </p>     
+            <Link to={'/ranking'} style={{ "textDecoration": "none" }}>
+                <button className={styles.myButton} >
+                    {"Recomendações de investimentos"}
+                </button>
+            </Link>       
+
         </div>
     );
 };
